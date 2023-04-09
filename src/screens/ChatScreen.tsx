2b67e7346108b465bd89eaff9932e33a46805c2f
Maxton Zhang <maxtonzh@usc.edu>
2023-04-09 12:48:43 -0700

    Converted chatscreen to use more flex boxes, added template for chat ui and textbox

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -6,17 +6,28 @@ import ChatBox from '../components/Chatbox'
 
 function ChatScreen() {
     return (
-        <div className={`${BG_STYLE}`}>
+        <div className={`${BG_STYLE} flex flex-col min-h-screen`}>
             <Navbar />
-            <div className="flex flex-col h-screen">
-                <button className="bg-secondary-200 text-white font-bold py-5 px-4 rounded-2xl w-1/6 shadow-lg ml-5 mb-5 mt-10">
-                    <FontAwesomeIcon icon={faAdd} className="mr-3" size="lg" />
-                    <span className="text-lg uppercase">Create a Chat</span>
-                </button>
-                <ChatBox />
-                <ChatBox />
-            </div>
+            <div className="flex flex-row flex-grow">
+                <div className="flex flex-col h-full w-1/6" id="chatList">
+                    <button className="bg-secondary-200 text-white font-bold py-5 px-4 rounded-2xl shadow-lg ml-5 mb-5 mt-10">
+                        <FontAwesomeIcon icon={faAdd} className="mr-3" size="lg" />
+                        <span className="text-lg uppercase">Create a Chat</span>
+                    </button>
+                    <ChatBox />
+                    <ChatBox />
+
+                </div>
+                <div className="flex flex-col flex-1 w-5/6 max-w-100 pl-10 pr-10">
+                    <div className="bg-quaternary-300 h-screen rounded-2xl mt-10 mb-10 h-full">
+                    </div>
+                    <div className="bg-quaternary-300 h-screen rounded-2xl mb-12 h-64">
+                    </div>
+
+                </div>
+            </div> 
         </div>
+
     )
 }
 
